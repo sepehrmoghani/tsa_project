dc798f38db2a8032cc1e5ee661188f05bff74bc0
Sepehr Moghani <100472497+sepehrmoghani@users.noreply.github.com>
2024-01-10 14:00:57 +0800

    Update settings.py
    updated ALLOWED_HOSTS

--- tsa_project/settings.py
+++ tsa_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l$eck)e58#7$0obj3o1%dtf81-i8vv3_oz!900cw7i1^kkkbyy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = 'home'
 
