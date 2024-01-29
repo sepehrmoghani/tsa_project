79160404972cecb26f49459cb6627cc55679a427
Sepehr Moghani <sepehr.moghani@hotmail.com>
2024-01-29 11:44:52 +0800

    updated ssh

--- tsa_project/settings.py
+++ tsa_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l$eck)e58#7$0obj3o1%dtf81-i8vv3_oz!900cw7i1^kkkbyy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.badbunch.com.au', 'badbunch.com.au', '3.106.128.163']
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = 'home'
 
