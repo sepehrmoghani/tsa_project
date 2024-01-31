291b833d45a81097f2398082ecd76acd2ab489e6
Sepehr Moghani <sepehr.moghani@hotmail.com>
2024-01-31 10:05:25 +0800

    fixed pdf to excel

--- pdf_to_excel/views.py
+++ pdf_to_excel/views.py
@@ -7,6 +7,8 @@ from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
 
 class HomePageView(LoginRequiredMixin, View):
     login_url = 'login'  # Redirect to the login page if not logged in
@@ -40,21 +42,41 @@ class PdfToExcelView(LoginRequiredMixin, View):
 
     def post(self, request):
         try:
-            the_pdf = request.POST.get("pdf_file")
-            start_page = int(request.POST.get("start_page"))
-            multiple_page = True if request.POST.get("multiple_page") == "true" else False
+            # Get the uploaded file from request.FILES
+            uploaded_file = request.FILES.get("pdf_file")
+            
+            # Check if a file was uploaded
+            if uploaded_file:
+                # Save the uploaded file temporarily
+                fs = FileSystemStorage()
+                saved_file = fs.save(uploaded_file.name, uploaded_file)
 
-            dataframes = self.read_pdf(the_pdf + ".pdf", start_page, multiple_page)
+                # Construct the file path
+                the_pdf = fs.path(saved_file)
+                
+                # Extract other form data
+                start_page = int(request.POST.get("start_page"))
+                multiple_page = request.POST.get("multiple_page") == "true"
 
-            if dataframes:
-                all_data = pd.concat(dataframes, ignore_index=True)
-                excel_file = the_pdf + ".xlsx"
-                all_data.to_excel(excel_file, index=False)
-                response = HttpResponse(content_type='application/vnd.ms-excel')
-                response['Content-Disposition'] = 'attachment; filename=' + excel_file
-                all_data.to_excel(response, index=False)
-                return response
+                # Read PDF and process data
+                dataframes = self.read_pdf(the_pdf, start_page, multiple_page)
+
+                if dataframes:
+                    all_data = pd.concat(dataframes, ignore_index=True)
+                    excel_file = saved_file + ".xlsx"
+                    excel_path = fs.path(excel_file)
+                    
+                    # Save data to Excel
+                    all_data.to_excel(excel_path, index=False)
+                    
+                    # Prepare response with Excel file
+                    with open(excel_path, 'rb') as excel_file:
+                        response = HttpResponse(excel_file.read(), content_type='application/vnd.ms-excel')
+                        response['Content-Disposition'] = 'attachment; filename=' + excel_file.name
+                    return response
+                else:
+                    return HttpResponse("No data to save.")
             else:
-                return HttpResponse("No data to save.")
+                return HttpResponse("No file uploaded.")
         except Exception as e:
             return HttpResponse(f"An error occurred: {e}")
