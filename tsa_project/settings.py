b203eae11b99affdc1827e5bc521d99badc56dce
Sepehr Moghani <100472497+sepehrmoghani@users.noreply.github.com>
2024-02-22 10:55:47 +0800

    Update settings.py

--- tsa_project/settings.py
+++ tsa_project/settings.py
@@ -130,6 +130,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Add the path to the directory where your static files are located
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
